6491431c5a2c265b4cb55c30eb6f400a6592f1d7
88SusD88 <is25suslinikovda@artcollege.ru>
2025-01-14 19:13:32 +0300

    Добавлен класс Producer

---
+++ WebApp/Models/Producer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+    }
+}
